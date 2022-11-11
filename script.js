e2ed83664ae70750c623a2ead35716ac4a837a75
Pedro Maia <70556699+PedroPMaia@users.noreply.github.com>
2022-11-11 00:10:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let isIgnite = false
+function changeCard(event) {
+  const card = event.currentTarget
+  const backgroundImage = isIgnite
+    ? 'url(./assets/bg-ignite.svg)'
+    : 'url(./assets/bg-explorer.svg)'
+  isIgnite = !isIgnite
+  card.style.backgroundImage = backgroundImage
+}
